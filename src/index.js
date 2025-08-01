918a8542f55c9996fd182a2cbe9333c8056cee69
Shivaraju NS <shivarajsmurthy@gmail.com>
2025-08-01 10:28:02 +0530

    fix: resolve blank page issue on GitHub Pages by setting correct homepage and deploying with gh-pages

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
