ee2aef468c8cddd0695ecfb2b1285f6e5bc3c332
Shivaraju NS <shivarajsmurthy@gmail.com>
2025-08-01 10:07:06 +0530

    added feature like add work, remove work to the to do list

--- src/listContainer.js
+++ src/listContainer.js
@@ -4,17 +4,32 @@ function List() {
 
     const[works, setWorks] = useState(["Work-1", "Work-2"])
 
+    const AddWork = ()=>{
+        let addWork = document.getElementById("textInput").value;
+        if(addWork.trim() !== ""){
+            setWorks(w => [...w, addWork])
+            document.getElementById("textInput").value =""
+        }
+        
+    }
+
+    const RemoveWork = (i)=> {
+        setWorks(works.filter( (_,index)=> i !== index ));
+    }
+
     return(
         <div className='lis-main-cont'>
             <div className='Input'>
                 <input type='text' placeholder='Add Works......' id="textInput"></input>
-                <button id="add-btn"> Add </button>
+                <button id="add-btn" onClick={AddWork}> Add </button>
             </div>
 
             <div className='list-cnt'>
                 <ol>
                     {works.map( (work, index)=>
-                    <li id="lists" key={index}>{work}</li> )}
+                    <div className='list-cnt-cross-btn'>
+                    <li id="lists" key={index}>{work} 
+                     </li><button onClick={()=>RemoveWork(index)} >x</button> </div>)}
                 </ol>
             </div>
         </div>
